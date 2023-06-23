eb2fb911ccc9a583052256a065bdec6f489bd84a
Nyuykisamo Lamfih <lamfihbilly@hotmail.com>
2023-06-23 10:45:12 +0100

    classes page completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-
-import React, { useState, useEffect } from "react"
-import Navbar from "@/scenes/navbar/index.tsx"
-import {SelectedPage} from "@/shared/types"
-import Home from "./scenes/home"
-import Benefits from "@/scenes/benefits"
-
+import React, { useState, useEffect } from "react";
+import Navbar from "@/scenes/navbar/index.tsx";
+import { SelectedPage } from "@/shared/types";
+import Home from "./scenes/home";
+import Benefits from "@/scenes/benefits";
+import OurClasses from "@/scenes/ourclasses";
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
-  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true)
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -16,21 +17,24 @@ function App() {
         setIsTopOfPage(true);
         setSelectedPage(SelectedPage.Home);
       }
-      if(window.scrollY !== 0) setIsTopOfPage(false)
-    }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
     window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll)
-  }, [])
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
-  <div className="app bg-gray-20 bg-gray-20">
-    <Navbar 
-    isTopOfPage={isTopOfPage} 
-    selectedPage={selectedPage} setSelectedPage={setSelectedPage}/> 
-   <Home setSelectedPage={setSelectedPage} />
-   <Benefits setSelectedPage={setSelectedPage} />
-
-  </div>
-  )
+    <div className="app bg-gray-20 bg-gray-20">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <Home setSelectedPage={setSelectedPage} />
+      <Benefits setSelectedPage={setSelectedPage} />
+      <OurClasses setSelectedPage={setSelectedPage} />
+      Conta
+    </div>
+  );
 }
 
-export default App
+export default App;
